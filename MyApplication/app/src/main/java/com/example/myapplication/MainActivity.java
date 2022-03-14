0c8d9c7cdebda62c5d3df2a67fb651ab59686414
AlOsaimiTalal <100706870+AlOsaimiTalal@users.noreply.github.com>
2022-03-14 18:06:35 +0300

    done

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Intent ticket = new Intent(MainActivity.this,MainActivity2.class);
+        EditText Name =findViewById(R.id.YourName);
+        EditText Age =findViewById(R.id.YourAge);
+        TextView emp = findViewById(R.id.ER);
+
+        Button fly = findViewById(R.id.transferring);
+
+        fly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Name.getText().toString().isEmpty() || Age.getText().toString().isEmpty()){
+                    emp.setText("Please check your name or age");
+                }
+                else {
+
+                    String N = Name.getText().toString();
+                    String A = Age.getText().toString();
+                    ticket.putExtra("simple", N);
+                    ticket.putExtra("simple1", A);
+
+                    startActivity(ticket);
+                }
+            }
+        });
+
+
+
+
+
+        }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        TextView mName =findViewById(R.id.MyName);
+        TextView mAge =findViewById(R.id.MyAge);
+
+        Bundle des = getIntent().getExtras();
+
+        String NameF = des.getString("simple");
+        String AgeF = des.getString("simple1");
+
+        mName.setText(NameF);
+        mAge.setText(AgeF);
     }
 }
